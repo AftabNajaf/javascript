7095ab331f9b0b6526efc36277fbb69491fc210e
Aftab_Najaf <aftabsial844@gmail.com>
2024-02-13 19:16:58 +0000

    type

--- basics/comparisons.js
+++ basics/comparisons.js
@@ -21,11 +21,26 @@
 
 const id=Symbol('123');
 const sId=Symbol('123');
-console.log(id==sId)
-console.table([id,sId])
+// console.log(id==sId)
+// console.table([id,sId])
 
 const myFun=function(){
     console.log('function');
 
 }
-console.log(typeof myFun);
+// console.log(typeof myFun);
+
+let youtubeName='aftab123';
+let anotherName=youtubeName;
+anotherName='afi123';
+// console.log(anotherName);
+// console.log(youtubeName);
+
+let userOne={
+    email:'afatb@gmail',
+    id:'1234'
+}
+let userTwo=userOne;
+userTwo.email='afi@google.com'
+console.log(userTwo.email);
+console.log(userOne.email);
