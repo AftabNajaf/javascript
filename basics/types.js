487c76e6bef344d6710933f27354d98dbcc600a0
Aftab_Najaf <aftabsial844@gmail.com>
2024-02-13 11:07:23 +0000

    types and variables

---
+++ basics/types.js
@@ -0,0 +1,9 @@
+"use strict";//treat all code as JS new version
+let name="aftab";//string
+let age=25;//number
+let isLogin=true;//boolean
+
+//null is standalone value,
+//symbol unique
+// null typef is object
+console.log(typeof null)
---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId=1234;
+let accountEmail="aftab@google.com";
+var accountPassword="12345";
+accountCity="Isb";
+let accountState;
+
+
+accountEmail='afi@google.com';
+accountPassword='67890';
+accountCity='jhang';
+accountState='punjab'
+
+// accountId=54321; not allowed
+//use only let 
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
