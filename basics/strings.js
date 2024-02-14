e61790c78aafba70b25397a6168158d6567f04c5
Aftab_Najaf <aftabsial844@gmail.com>
2024-02-14 09:19:21 +0000

    strings

---
+++ basics/strings.js
@@ -0,0 +1,17 @@
+const name ='aftab';
+const age=26;
+
+// console.log(`my name is ${name} and my age is ${age}`);
+const gameName= new String('puzzel')
+console.log(gameName[0]);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('l'));
+console.log(gameName.substring(1,3));
+
+const trim='       teste   ';
+console.log(trim);
+
+const url='https://afi.com/aftab%sial'
+console.log(url.replace('%','/' ))
